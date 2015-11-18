f29938389b683e8c130de5ea806ae7df59fa1227
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-11-17 19:01:50 -0800

    they don't

--- 4-supercount.js
+++ 4-supercount.js
@@ -2,7 +2,7 @@ function superCount(string){
 	this.words = string.split(' ').length;
 	this.chars = string.length;
     this.spaces =  string.match(/ /g).length;
-	 this.avgLength = (this.chars - this.spaces)/this.words
+	this.avgLength = (this.chars - this.spaces)/this.words;
 }
 
 var superCounter = new superCount("count me in")
